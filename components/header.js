96b3e08a33df2a50f4c8af8711dc60c8b968bcd5
Nipunraj <nipunraj@mozilor.com>
2024-05-02 14:35:58 +0530

    cookieyes

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 let headLinks = `
+<script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/55f1f3be99dd1bc57d3ac597/script.js"></script>
 <link
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
   rel="stylesheet"
@@ -16,6 +17,7 @@ let headLinks = `
   integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
   crossorigin="anonymous"
 ></script>
+
 `;
 
 document.querySelector("#html-head").insertAdjacentHTML("beforeend", headLinks);
